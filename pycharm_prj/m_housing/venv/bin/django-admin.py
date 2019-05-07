fc6b8db538e6efbdb092c2fb57c1d1a854ae8744
alexandradilja <alexandrab18@ru.is>
2019-05-07 10:59:17 +0000

    Initial commit

---
+++ pycharm_prj/m_housing/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/alexandradilja/Documents/HR/vor_19/verklegt_2/pycharm_prj/m_housing/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
